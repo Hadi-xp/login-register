f159374f21654a09feabb68af6ae734e11bda644
Hadixp <hadi.b.2002@gmail.com>
2025-03-09 00:16:15 +0330

    added npm modules

--- Back/index.js
+++ Back/index.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const app = express();
+app.use(express.json);
+const config = require('config');
+const {body,validationResault} = require('express-validator');
+
+
+
+
+
+
+
+
+app.listen(config.get('serverPort'),()=>{console.log(`server is runing on port ${config.get('serverPort')}`)});
