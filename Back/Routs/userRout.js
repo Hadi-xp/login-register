a7bff931ffc75db25779e15c7c3df63b28bf0eb2
Hadixp <hadi.b.2002@gmail.com>
2025-03-11 00:07:39 +0330

    nodemon added

--- Back/Routs/userRout.js
+++ Back/Routs/userRout.js
@@ -5,22 +5,24 @@ const userRouter = express.Router();
 
 // Get API
 userRouter.get('/getUser',(req,res)=>{
-    res.json({mag:'users'})
+    res.json({msg:'getting Users'})
 })
 
 // Post API
 userRouter.post('/postUser',(req,res)=>{
-
+    res.json({mag:'posting Users'})
 })
 
 // Put API
 userRouter.put('/putUser',(req,res)=>{
+    res.json({mag:'putting Users'})
 
 })
 
 // Delete API
 userRouter.delete('/deleteUser',(req,res)=>{
-    
+    res.json({mag:'deleting Users'})
+
 })
 
 
