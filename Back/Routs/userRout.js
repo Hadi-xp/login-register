50b812121940770042b96aabdb2239a7bd215aa3
Hadixp <hadi.b.2002@gmail.com>
2025-03-10 23:35:02 +0330

    added Rest API

--- Back/Routs/userRout.js
+++ Back/Routs/userRout.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const userRouter = express.Router();
 
+
+
+// Get API
 userRouter.get('/getUser',(req,res)=>{
     res.json({mag:'users'})
+})
+
+// Post API
+userRouter.post('/postUser',(req,res)=>{
+
+})
+
+// Put API
+userRouter.put('/putUser',(req,res)=>{
+
+})
+
+// Delete API
+userRouter.delete('/deleteUser',(req,res)=>{
+    
 })
 
 
