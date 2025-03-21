80cd0e952cf9fa9a4373b4cbb515d6f009ecee08
Hadixp <hadi.b.2002@gmail.com>
2025-03-21 15:15:13 +0330

    schema made

--- Back/index.js
+++ Back/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/newLogin')
+.then(()=>{console.log('connected to data base');})
+.catch(()=>{console.log('not connected');})
 app.use(express.json());
 const config = require('config');
 const {body,validationResault} = require('express-validator');
--- Back/schema/schema.js
+++ Back/schema/schema.js
@@ -1,1 +1,4 @@
 const mongoose = require('mongoose');
+new mongoose.Schema({
+    
+})
