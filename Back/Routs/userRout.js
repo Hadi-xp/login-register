ecb5abac15b69d31d2d06ab214f14ea188399932
Hadixp <hadi.b.2002@gmail.com>
2025-03-21 15:22:57 +0330

    schema and modle made

--- Back/Routs/userRout.js
+++ Back/Routs/userRout.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userRouter = express.Router();
+const userSchema = require('../schema/schema');
 
 
 
--- Back/schema/schema.js
+++ Back/schema/schema.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
-new mongoose.Schema({
-    
+const userSchema = new mongoose.Schema({
+    Name:{type:String,require:true},
+    Email:{type:String,require:true},
+    Balance:{type:Number},
+    isAdmin:{type:Boolean}
 })
+
+const User = mongoose.model('User',userSchema);
+module.exports = User
