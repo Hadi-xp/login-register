c7a9a69db648135dd1cdd5e6cff36d2e6b2056f7
Hadixp <hadi.b.2002@gmail.com>
2025-04-14 17:16:58 +0430

    added: validations

--- Back/Routs/userRout.js
+++ Back/Routs/userRout.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const userRouter = express.Router();
-const validate = require('../auth/authentication');
+// const validate = require('../auth/authentication');
 // const userSchema = require('../schema/schema');
 const User = require('../schema/schema');
+const {body,validationResult} = require('express-validator');
 
 
 
@@ -12,7 +13,16 @@ userRouter.get('/getUser',(req,res)=>{
 })
 
 // Post API
-userRouter.post('/postUser',validate.registerValidation(),async (req,res)=>{
+userRouter.post('/postUser',[
+    body('Email','email must be valid').isEmail().isEmpty(),
+    body('Name','name cant be empty').notEmpty(),
+    body('Password','password cant be empty').notEmpty()
+],async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(404).json({data:null,errors:errors.array()})
+
+    }
     let newUser = new User({
         Name:req.body.Name,
         Email:req.body.Email,
