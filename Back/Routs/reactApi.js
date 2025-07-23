1735ec0a5c755d3480dfaf48efb624d0d99551d6
Hadixp <hadi.b.2002@gmail.com>
2025-07-23 21:33:59 +0430

    Update reactApi.js

--- Back/Routs/reactApi.js
+++ Back/Routs/reactApi.js
@@ -2,9 +2,8 @@ const express = require('express');
 const reactRouter = express.Router();
 
 reactRouter.get('/getData',(req,res)=>{
-    res.json([
-        data={first_name:'hadi',last_name:'bahadori'}
-    ])
+    res.json({data:data=[{first_name:'hadi',last_name:'bahadori'},{first_name:'ahmad',last_name:'kohan'}]}
+    )
 })
 
 
