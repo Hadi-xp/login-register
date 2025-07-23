045e458648d67aae535b3f9acaac809657e4cfb1
Hadixp <hadi.b.2002@gmail.com>
2025-07-23 23:05:59 +0430

    Update reactApi.js

--- Back/Routs/reactApi.js
+++ Back/Routs/reactApi.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const reactRouter = express.Router();
 
-reactRouter.get('/getData',(req,res)=>{
-    res.json({data:data=[{first_name:'hadi',last_name:'bahadori'},{first_name:'reza',last_name:'ahmadi'}]})
+reactRouter.post('/getData',(req,res)=>{
+    const secondData = req.body;
+    const objectCheck = Object.keys(secondData).length
+      if(objectCheck === 0){
+        res.json({data:data=[{first_name:'hadi',last_name:'bahadori'},{first_name:'reza',last_name:'ahmadi'}]})
+    }else{
+        res.json({data:data=[{first_name:'hadi',last_name:'bahadori'},{first_name:'reza',last_name:'ahmadi'},{first_name:secondData.first_name,last_name:secondData.last_name}]})
+    }
 })
 
 
